570e55f92d9c321807d6d7ebcd8071c71d9e5bbc
hungmanh02 <domanh462@gmail.com>
2022-05-11 12:14:37 +0700

    câu 1 Thuật toán: Sử dụng ngôn ngữ lập trình bất kỳ để giải quyết các bài toán dưới đây  a .Cho mảng số nguyên chưa được sắp xếp. Viết hàm xếp 5 số lớn nhất ra đầu mảng.

---
+++ findmax5.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+ function findMax5($b){
+     rsort($b);
+    for($c=0;$c<count($b);$c++){
+        if($c<5){
+            print_r($b[$c] ." ") ;
+             
+        }
+       
+    }
+    
+     
+ }
+ $b = [14,12,38,17,10,36,12,29,45,34,48,22];  
+ print_r(findMax5($b));
+
+?>
