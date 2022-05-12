dbeec729f9c3922b2065c3f7f77bf5c8e9792c3a
hungmanh02 <domanh462@gmail.com>
2022-05-12 08:05:22 +0700

    commit lại file form_contact.php

--- form_contact.php
+++ form_contact.php
@@ -10,7 +10,7 @@
         $phone=$_POST['phone'];
         $content=$_POST['content'];
         if(!empty($fullname) && !empty($email) &&!empty($phone) && !empty($content)){
-            print_r($_POST);
+            
             
             $sql="INSERT INTO  `contact`   (`fullname`,`email`,`phone`,`content`) VALUES ('$fullname','$email','$phone','$content')";
             if($conn->query($sql)===TRUE){
